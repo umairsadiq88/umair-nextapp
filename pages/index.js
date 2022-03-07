19fad9dee15d0e7d3b99d9e14e649b530c7443e8
umairsadiq88 <umairsadiq2010@gmail.com>
2022-03-07 12:35:25 +0500

    update head for google verificataion

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,12 @@ const index = () => {
         <title>Home Page </title>
         <meta name="description" content="Free Web youtube tutorials" />
         <meta name="keywords" content="HTML, CSS, JavaScript, next.js" />
-        <meta name="author" content="thapa technical" />
+        <meta name="author" content="umair sadiq" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"></meta>
+          
+          <meta name="google-site-verification" content="zTo923SWkYMp778kttcb5wEGR-2MHSIsh8uWU1u1HBg" />
       </Head>
       <Header />
     </>
