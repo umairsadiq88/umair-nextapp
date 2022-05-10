bec4ef5b680e2afb0c7516566fe2226ddd5e0a1a
Umair Sadiq <59564704+umairsadiq88@users.noreply.github.com>
2022-05-10 20:29:05 +0500

    homejs

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,3 @@
-// So how is the routing in next.js app
-
-// =>File-system based routing mechanism
-
-//     => When a file is added to the pages folder in a project,
-//         by default all the files becomes available as a route.
 
 const home = () => {
     return (
